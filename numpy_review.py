5db6edceb2ca1079053ba350477ae9a22c69cf60
brendon250891 <brendon.willoughby@gmail.com>
2020-01-06 09:50:19 +1100

    Added date conversion

--- numpy_review.py
+++ numpy_review.py
@@ -1,9 +1,8 @@
 import matplotlib.pyplot as plt
-import matplotlib.dates as matdates
+import matplotlib.dates as plt_dates
 import numpy as np
 import sqlite3
 import datetime
-import openpyxl
 
 DATABASE_NAME = "review_data.db"
 
@@ -18,6 +17,7 @@ def run_database_query(query, params=None):
     connection.close()
     return query_results
 
+
 def get_top_three_brands_total_reviews():
     """ Gets the top three brands total reviews by first getting the names and then getting the monthly reviews for
     each"""
@@ -26,8 +26,9 @@ def get_top_three_brands_total_reviews():
         select i.brand from items i join reviews r on r.asin = i.asin group by i.brand order by count() desc limit 3
     """
     monthly_reviews_query = """
-        select strftime('%m-%Y', r.review_date) as date, count() from reviews r join items i on i.asin = r.asin 
-        where i.brand = ? group by date order by strftime('%Y', r.review_date), strftime('%m', r.review_date)
+        select strftime('%m', r.review_date) || "-" || substr(strftime('%Y', r.review_date), 3, 4) as date, count() 
+        from reviews r join items i on i.asin = r.asin where i.brand = ? group by date 
+        order by strftime('%Y', r.review_date), strftime('%m', r.review_date)
     """
     for brand in run_database_query(top_three_brands_query):
         monthly_reviews = run_database_query(monthly_reviews_query, tuple(brand))
@@ -72,7 +73,24 @@ def create_n_dimensional_array(n, data):
     return nd_array
 
 
-""" Need to plot """
+def datetime_string_to_datetime(dates):
+    for d in dates:
+        d = datetime.datetime.strptime(d, '%m-%y')
+    return dates
 
-for key, value in get_top_five_brands_average_rating_per_month_2017_2019().items():
-    print(key + " : " + str(value))
+
+def generate_plot(plot, data):
+    for value in data.values():
+        value[0] = datetime_string_to_datetime(value[0])
+        plot.plot(value[0], value[1])
+    plot.tick_params(axis='x', labelsize=6, rotation=90)
+    #plot.set_xlim(datetime.date(2005, 1, 1), datetime.date(2020, 1, 1))
+    #plot.xaxis.set_major_locator(plt_dates.YearLocator())
+    #plot.xaxis.set_minor_locator(plt_dates.MonthLocator())
+    plot.legend(data.keys())
+   #plot.xaxis_date()
+    #plot.xaxis.set_major_formatter(plt_dates.DateFormatter('%m-%y'))
+
+
+generate_plot(plt.subplot(2, 1, 1), get_top_three_brands_total_reviews())
+plt.show()
